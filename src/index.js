466288f62f7bdfdbc28413a4992d0e16872ece50
Xin <xinchents@gmail.com>
2024-04-06 20:08:38 -0400

    updating js, setting variables and defining function

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
 const startButton = document.querySelector('#start');
 // TODO: Add the missing query selectors:
-const score; // Use querySelector() to get the score element
-const timerDisplay; // use querySelector() to get the timer element.
+const score = document.querySelector('#score'); // Use querySelector() to get the score element
+const timerDisplay = document.querySelector('#timer'); // use querySelector() to get the timer element.
 
 let time = 0;
 let timer;
@@ -21,7 +21,7 @@ let difficulty = "hard";
  *
  */
 function randomInteger(min, max) {
-  // return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
@@ -41,7 +41,14 @@ function randomInteger(min, max) {
  */
 function setDelay(difficulty) {
   // TODO: Write your code here.
-  
+  if (difficulty === "easy") {
+    return 1500;
+  } else if (difficulty === "normal") {
+    return 1000;
+  } else if (difficulty === "hard") {
+    // Generate a random integer between 600 and 1200
+    return randomInteger(600, 1200);
+  } 
 }
 
 /**
@@ -60,6 +67,14 @@ function setDelay(difficulty) {
  */
 function chooseHole(holes) {
   // TODO: Write your code here.
+  const randomHoles = randomInteger(0,8);
+  const hole = holes[randomHoles];
+  if (hole === chooseHole.lastHole){
+    return chooseHole(holes);
+  }
+  chooseHole.lastHole = hole;
+
+  return hole;
 
 }
 
